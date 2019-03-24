c8f617f029f9dee49d98a2891a14da1391d33026
Javan R <mail@javan.de>
2019-03-24 10:13:57 +0100

    fix debloy bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
-  production: true,
-  apiURL: '//localhost:4200/api'
+    production: true,
+    apiURL: 'https://honey-pi.de/backend/',
+    firebase: {
+        apiKey: 'AIzaSyCifK9_mBsoY7VZHbMSb-VRa2iQMh41az8',
+        authDomain: 'honeypi-push-notifications.firebaseapp.com',
+        databaseURL: 'https://honeypi-push-notifications.firebaseio.com',
+        projectId: 'honeypi-push-notifications',
+        storageBucket: 'honeypi-push-notifications.appspot.com',
+        messagingSenderId: '969621478785'
+    }
 };
